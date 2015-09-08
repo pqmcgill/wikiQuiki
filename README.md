3b2c3f3861fb6d5ce0c9022e8be70c1de168a930
pqmcgill <Gocolts90!>
2015-09-08 13:35:23 -0500

    initial hello world commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # wikiQuiki
 A game in which you race against your friends via wikipedia articles
+
+# installation
+install webpack globally
+
+    npm install -g webpack
+
+
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	entry: './src/App.js',
+	output: {
+		path: './public',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, loader: 'babel'}
+		]
+	}
+};
